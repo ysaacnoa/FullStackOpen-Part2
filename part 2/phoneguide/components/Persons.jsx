4e7aa709c4246b13318245e1a24b252d701ca6eb
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2023-12-26 16:39:37 -0500

    phoneguide

--- part 2/phoneguide/components/Persons.jsx
+++ part 2/phoneguide/components/Persons.jsx
@@ -5,13 +5,18 @@ export default function Persons ({persons, filterPerson, setPersons}) {
   
   if(!persons) return
   
-  const toggleDelete = (id) => () => {
-    personServices
-      .removePerson(id)
-      .then(() => {
-        const newList = persons.filter(person => person.id !== id)
-        setPersons(newList)
-      })
+  const toggleDelete = (filteredPerson) => () => {
+    const wantDelete = window.confirm(`Delete ${filteredPerson.name} ?`)
+
+    if(wantDelete){
+      personServices
+        .removePerson(filteredPerson.id)
+        .then(() => {
+          const newList = persons.filter(person => person.id !== filteredPerson.id)
+          setPersons(newList)
+        })
+    }
+  
   }
   return (
     <p>
@@ -22,7 +27,7 @@ export default function Persons ({persons, filterPerson, setPersons}) {
         .map((filteredPerson) => (
           <li key={filteredPerson.id}>
             {filteredPerson.name} {filteredPerson.number}
-            <button onClick={toggleDelete(filteredPerson.id)}>delete</button>
+            <button onClick={toggleDelete(filteredPerson)}>delete</button>
           </li>
         ))}
     </p>
